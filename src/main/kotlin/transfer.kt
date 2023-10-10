e6bd1b95224a1ca343bdfb930a7c218d4a514fe4
OperaJAVA <git@github.com:OperaJAVA/HelloKotlin.git>
2023-10-10 23:54:12 +0300

    first2 commit

--- src/main/kotlin/transfer.kt
+++ src/main/kotlin/transfer.kt
@@ -2,10 +2,10 @@ fun main() {
 
     // Вводим сумму перевода
     print("Введите цифрами сумму в рублях: ")
-    val amount=readln().toInt()
+    val amount = readln().toInt()
 
     // Вычисляем размер комиссии
-    val commission=if (amount < 300 || amount > 75_000)
+    val commission = if (amount < 300 || amount > 75_000)
         ((amount + 35) * 0.75) / 100
     else
         if (amount > 75_000) (amount * 0.6) / 100 + 20
